74c62264d42eb396e79c6f4f9fe35c59ec57414b
Jose Herrera <jherrera892016@gmail.com>
2022-09-16 20:50:35 -0500

    add (core) Add initial Project
    - Add Initial project and Activity 2.

---
+++ app/src/main/java/com/example/cursoandroid/ActivityPrueba.kt
@@ -0,0 +1,11 @@
+package com.example.cursoandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityPrueba : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prueba)
+    }
+}
