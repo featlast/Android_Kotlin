b13bf4bdce0d65537a9e792705ec453868db3fc0
Jose Herrera <jherrera892016@gmail.com>
2022-10-26 22:06:02 -0500

    add(core): Add several functions.
    -Add Activity, Button, Edit Text, Layouts, .

--- app/src/main/java/com/example/cursoandroid/ActivityPrueba.kt
+++ app/src/main/java/com/example/cursoandroid/ActivityPrueba.kt
@@ -7,5 +7,7 @@ class ActivityPrueba : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_prueba)
+
+
     }
 }
--- app/src/main/java/com/example/cursoandroid/MainActivity.kt
+++ app/src/main/java/com/example/cursoandroid/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.cursoandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Método Btn 2
+        val btnCambiarColor : Button = findViewById(R.id.btn_secundario)
+        //agregar un listener
+        btnCambiarColor.setOnClickListener{
+            val txtPrincipal: TextView = findViewById(R.id.tv_txt_principal)
+            txtPrincipal.setTextColor(android.graphics.Color.BLUE)
+        }
+
+    }
+    /*Método del btn 1*/
+    fun cambiarColor (view:View){
+    val txtPrincipal: TextView = findViewById(R.id.tv_txt_principal)
+        txtPrincipal.setTextColor(android.graphics.Color.GREEN)
+    }
+
+    /*Edit text*/
+    fun cambiarTexto (view: View){
+    val etPrincipal: EditText = findViewById(R.id.et_txt_principal)
+        val txtPrincipal: TextView = findViewById(R.id.tv_txt_principal)
+        txtPrincipal.setText(etPrincipal.text)
     }
 }
